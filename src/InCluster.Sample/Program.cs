6455dc11cde6f1d0d0c8cd66451b048b27ec09ac
unknown <luhamter09@W00133814.officesg.local>
2024-07-12 08:52:28 +0700

    rename lib

--- src/InCluster.Sample/Program.cs
+++ src/InCluster.Sample/Program.cs
@@ -7,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services.AddEz4LeaderConfig(op =>
 {
@@ -17,13 +16,6 @@ builder.Services.AddEz4LeaderConfig(op =>
 builder.Services.AutoApplyEz4CronJob();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseAuthorization();
 
